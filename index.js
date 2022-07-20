02abb137eba5d5710cc83b141aa05f66c3b82e0d
JessyFranco <jesicafranco_1518@outlook.es>
2022-07-20 20:40:13 -0300

    di formato

--- index.js
+++ index.js
@@ -8,45 +8,45 @@ listaUsRegistrados.push(usuario1, passwordLogin1);
 while (iniciarSesion != "") {
     if (iniciarSesion == "in") {
         let usuario = prompt("Ingrese su usuario:");
-        if (listaUsRegistrados.includes( `${usuario}`)){
+        if (listaUsRegistrados.includes(`${usuario}`)) {
             let password = parseInt(prompt("Ingrese su contraseña: "));
-            if (listaUsRegistrados.includes(`${password}`)){
+            if (listaUsRegistrados.includes(`${password}`)) {
                 alert(`Login exitoso, Bienvenida/o, siga con su compra!!!`);
             }
         }
-        else{
+        else {
             alert(`El usuario es incorrecto, pruebe nuevamente`);
             continue
         }
     }
 
     else if (iniciarSesion == "re") {
-    class registroUsuario {
-        constructor(nombre, apellido, edad, direccion, usuario, password) {
-            this.nombre = nombre;
-            this.apellido = apellido;
-            this.edad = edad;
-            this.direccion = direccion;
-            this.usuario = usuario;
-            this.password = password;
+        class registroUsuario {
+            constructor(nombre, apellido, edad, direccion, usuario, password) {
+                this.nombre = nombre;
+                this.apellido = apellido;
+                this.edad = edad;
+                this.direccion = direccion;
+                this.usuario = usuario;
+                this.password = password;
+            }
         }
-    }
-    const registro = new registroUsuario(prompt("ingrese su nombre"), prompt("Ingrese su apellido"), parseInt(prompt("ingrese su edad")), prompt("Ingrese su direccion"), prompt("Ingrese un usuario"), prompt("Ingrese una contraseña"));
-    alert(`Se registro exitosamente`);
-    listaUsRegistrados.push(registro);
-    let usuario = prompt("Ingrese su usuario:");
-    let password = parseInt(prompt("Ingrese su contraseña: "));
-    alert(`Bienvenido/a ${usuario}`)
-    /* Error, no me pushea mis nuevos registros en la lista, solo aparecen los que pushee al principio */
-    /* alert(` ${listaUsRegistrados}`) */
-    break
+        const registro = new registroUsuario(prompt("ingrese su nombre"), prompt("Ingrese su apellido"), parseInt(prompt("ingrese su edad")), prompt("Ingrese su direccion"), prompt("Ingrese un usuario"), prompt("Ingrese una contraseña"));
+        alert(`Se registro exitosamente`);
+        listaUsRegistrados.push(registro);
+        let usuario = prompt("Ingrese su usuario:");
+        let password = parseInt(prompt("Ingrese su contraseña: "));
+        alert(`Bienvenido/a ${usuario}`)
+        /* Error, no me pushea mis nuevos registros en la lista, solo aparecen los que pushee al principio */
+        /* alert(` ${listaUsRegistrados}`) */
+        break
     }
     else {
         alert(`La entrada ingresada no es valida`)
         let iniciarSesion = prompt("Para realizar su compra debe iniciar sesion, si ya tiene usuario ingrese (in), sino registrese con (re)");
         continue
     }
-break
+    break
 }
 
 let productos = prompt("Ingrese (p) si desea comprar plantas, (m) si desea comprar macetas o (i) si desea comprar insumos");
